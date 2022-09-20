0b5da6f86d609932ce30d2b49290f5e09981e119
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-20 04:27:10 +0200

    dohvat datoteke iz memorije

--- src/index.js
+++ src/index.js
@@ -2,15 +2,36 @@
 //req.query - objekt koji sadrži sve parametre
 //res - ispis u browser ide preko res , .send ispisuje string
 
-
+import storage from "./memory_storage"
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = 3000 
+
+app.get('/post', (req, res) => {
+    let posts = storage.posts 
+    let query = req.query
+
+    if (query.title) {
+        posts = posts.fillter(e.title.indexOf (query.title) >= 0)
+    }
+    if (query.createBy) {
+        posts = posts.fillter(e.createBy.indexOf (query.createBy) >= 0)
+    }
+
+    if (query._any) {
+        let terms = query._any.split(" ")
+        posts = posts.fillter(doc => {
+            let info = doc.title + " " + doc.createBy
+            return terms.every(term => info.indexOf(term) >= 0)
+        })
+    }
+    res.json(posts)
+})
 
 app.get('/', (req, res) => {
 
-    console.log(req.query)
+    let postovi = storage.post
 
     res.send('Suma je: ${suma} ') 
     console.log('Hello world') 
