d539c7c4fb19861c10a8429e6155164d770732c0
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-20 04:44:56 +0200

    memory

--- src/memory_storage.js
+++ src/memory_storage.js
@@ -1,7 +1,13 @@
 // Datoteka: src/memory_storage.js
 let storage = {
     posts: [
-        
+        {
+            id: 1001,
+            createdBy: "nina@gmail.com",
+            postedAt: "157",
+            source: "http://galerijaporec.hr/wp-content/uploads/2019/10/Galerija-Porec-Slika1.jpg",
+            title: "Porec galerija",
+        },
     ]
-    }
+   }
 export default storage;
